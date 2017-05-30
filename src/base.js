852573b2d7b540d612f6f24309429a4f529fcf81
Matt Griffis <mgriffis@advantagequickbase.com>
2017-05-30 13:30:54 -0600

    BaseHelpers.timeOfDayToString | Bug fix | 12PM is 12PM, not 12AM.

--- src/base.js
+++ src/base.js
@@ -1808,7 +1808,7 @@ var BaseHelpers = {
 
     var hours = timeOfDay.getHours().toString();
     var minutes = timeOfDay.getMinutes().toString();
-    var ampm = hours > 12 ? "pm" : "am";
+    var ampm = hours >= 12 ? "pm" : "am";
 
     hours = hours % 12;
     hours = hours ? hours : 12; // the hour '0' should be '12'
